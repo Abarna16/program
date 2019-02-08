6304521d0e05f09c4c8def337a863bbba160f8c6
Abarna16 <47447958+Abarna16@users.noreply.github.com>
2019-02-08 21:43:21 +0530

    Create 12.c

---
+++ 12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,rem=0;
+    int x=n;
+    scanf("%d",&n);
+     while(n!=0)
+     {
+         rem=rem*10 +(n%10);
+         n=n/10;
+     }
+    if(x==rem)
+    printf("Yes");
+    else
+    printf("No");
+    return 0;
+}
